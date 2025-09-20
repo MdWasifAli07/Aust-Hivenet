d6c390bfde3deb60379b4d78477a43f1f0a42e94
Ma-Huan Sheikh Meem <cenationmaster02@gmail.com>
2025-09-20 18:31:14 +0600

    Update GuestLayout.jsx

--- hivenet/resources/js/Layouts/GuestLayout.jsx
+++ hivenet/resources/js/Layouts/GuestLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from '@/Components/floating-navbar-demo'; // Import the Navbar component
 import BackgroundFX from '@/Components/BackgroundFX'; // Import BackgroundFX
+import Footer from '@/Components/Footer'; // Import Footer
 
 export default function GuestLayout({ children }) {
   return (
-    <div className="relative min-h-screen flex flex-col items-center">
+    <div className="relative min-h-screen flex flex-col">
       {/* BackgroundFX Component: Now placed outside content and fills the whole screen */}
       <BackgroundFX /> 
 
@@ -14,8 +15,13 @@ export default function GuestLayout({ children }) {
       </div>
 
       {/* Main Content Area */}
-      <div className="relative top-20 mt-6 w-full px-6 py-4 sm:max-w-xl sm:rounded-lg">
+      <div className="relative flex-grow top-10 mt-10 w-full px-6 sm:max-w-xl mx-auto sm:rounded-lg"> {/* Adjusted margin-top and centered content */}
         {children} {/* The children (form, etc.) will go here */}
+      </div> 
+
+      {/* Footer */}
+      <div className="relative z-30 w-full mt-12"> {/* Increased margin-top for footer */}
+        <Footer /> {/* Add the Footer component here */}
       </div>
     </div>
   );
